959e16e64582487539d2b54d8a9c6ffdbd17f516
afifao1 <afifao1>
2025-04-20 00:02:55 +0500

    MINOR: added controller,model,migrations

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
     $users = \App\Models\User::all();
     return view('welcome', ['users' => $users]);
 });
+
+Route::resource('authors',AuthorController::class);

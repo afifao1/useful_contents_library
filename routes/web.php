ce59bb55dd0caef5ebe40ecbdcce633ac4a77cfd
afifao1 <afifao1>
2025-05-01 00:32:25 +0500

    MINOR: Breeze worked with login register added, and bugs fixed

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,31 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContentController;
 use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+
 
 Route::get('/', [HomeController::class, 'home']);
 Route::resource('authors', AuthorController::class);

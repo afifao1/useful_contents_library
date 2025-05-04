bfea07ad318b6d3395470b9f4602262e59a5cdaa
afifao1 <afifao1>
2025-05-04 16:14:56 +0500

    PATCH: added api genres and user

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -6,5 +6,5 @@ use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
-    //
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,34 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+Route::post('/tokens/create', function (Request $request) {
+    $validated = $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8',
+    ]);
+
+    $user = User::create([
+        'name' => $validated['name'],
+        'email' => $validated['email'],
+        'password' => bcrypt($validated['password']),
+    ]);
+
+    $token = $user->createToken($validated['name']);
+
+    return response()->json([
+        'user' => $user->name,
+        'token' => $token->plainTextToken,
+    ]);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/genres',function(){
+    return \App\Models\Genre::all();
+})->middleware('auth:sanctum');
+
